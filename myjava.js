9d38a8738938918f54a867f303d469191f80e273
p <dezutterpablo@gmail.com>
2019-03-22 15:01:54 +0100

    innerHtml javascript toevoegen

--- myjava.js
+++ myjava.js
@@ -2,5 +2,14 @@ function pageload(){
   document.getElementById('doge').textContent="Woof";
 }
 function onhover(){
-  document.getElementById('doge').addEventListener(onmouseover, image src="doggie.png");
+  document.getElementById('doge').addEventListener("mouseover", mouseOver);
+  document.getElementById('doge').addEventListener("mouseout",mouseOut);
+
+    function mouseOver (){
+        document.getElementById('doge').innerHtml="doggie.png";
+        }
+
+    function mouseOn (){
+        document.getElementById('doge').innerHtml="Woof";
+
 }
