1a6fa32bb3b9f5b020310f22f2f177e8d2786532
p <dezutterpablo@gmail.com>
2019-03-22 12:29:18 +0100

    code javascript toevoegen

--- myjava.js
+++ myjava.js
@@ -1,3 +1,6 @@
 function pageload(){
   document.getElementById('doge').textContent="Woof";
 }
+function onhover(){
+  document.getElementById('doge').addEventListener("mouseover").img src="doggie.png";
+}
