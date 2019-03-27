b461d033a0a4eebba4f5c637e5f44bcf967e7633
p <dezutterpablo@gmail.com>
2019-03-27 12:32:54 +0100

    stapvoorstap javascript instoppen

--- myjava.js
+++ myjava.js
@@ -1,6 +1,9 @@
+var buttonDoge=document.getElementById("doge");
+//waarde van document
+
+
 
     //When the page is finished loading, change the text to "Woof!"
-
 
 document.getElementById("doge").innerHTML="Woof!";
 
@@ -8,12 +11,17 @@ document.getElementById("doge").innerHTML="Woof!";
 
     //When you hover over the button "Woof!" should disappear and a little doggo icon should appear instead
 
-var imageDoggo;
-image.src="doge.png"
+buttonDoge.addEventListener("click",showImage);
 
-document.getElementById("doge").
+function showImage (){
+console.log ("ikhebeenfotolatenzien");
+buttonDoge.innerHTML="ikhebeenfotolatenzien";
+//foto in button stoppen
 
+}
 
 
     //When you click the button a picture of a doge should show up somewhere on the page
+
+
     //When you click the button again, the picture should disappear
